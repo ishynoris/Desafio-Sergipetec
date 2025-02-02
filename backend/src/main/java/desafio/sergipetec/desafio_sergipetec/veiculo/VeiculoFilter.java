efa83b57484bb696b8e39e76d21d7aa9cc6eccfb
Anailson Mota [W10] <anailsonmota@moobi.com.br>
2025-02-02 01:07:48 -0300

    Retorna os valores opicionais dos filtros de veículo

--- backend/src/main/java/desafio/sergipetec/desafio_sergipetec/veiculo/VeiculoFilter.java
+++ backend/src/main/java/desafio/sergipetec/desafio_sergipetec/veiculo/VeiculoFilter.java
@@ -1,10 +1,7 @@
 package desafio.sergipetec.desafio_sergipetec.veiculo;
 
 import java.util.HashMap;
-
-import javax.management.InvalidAttributeValueException;
-
-import org.springframework.data.jpa.domain.Specification;
+import java.util.Optional;
 
 public class VeiculoFilter {
 
@@ -14,39 +11,29 @@ public class VeiculoFilter {
 		this.filter = filter;
 	}
 
-	public Specification<Veiculo> getSpecs() throws InvalidAttributeValueException, NumberFormatException {
-		Specification<Veiculo> specs = Specification.where(null);
-
-		if (this.filter.containsKey("mdo_id")) {
-			var param = Integer.parseInt(this.filter.get("mdo_id"));
-			specs = specs.and(this.withModeloId(param));
-		}
-
-		if (this.filter.containsKey("vco_tipo")) {
-			var codigo = Integer.parseInt(this.filter.get("vco_tipo"));
-			var param = VeiculoEnum.parse(codigo);
-			specs = specs.and(this.withTipo(param));
-		}
-
-		if (this.filter.containsKey("vco_ano")) {
-			var param = Integer.parseInt(this.filter.get("vco_ano"));
-			specs = specs.and(this.withAno(param));
-		}
-		return specs;
+	public Optional<Integer> getVeiculoId() {
+		var moduloId = this.filter.containsKey("vco_id")
+			? Integer.parseInt(this.filter.get("vco_id"))
+			: null;
+		return Optional.ofNullable(moduloId);
 	}
 
-	public Specification<Veiculo> withModeloId(Integer modeloId) {
-		return (root, query, builder) -> {
-			var expression = root.get("modelo").get("id");
-			return builder.equal(expression, modeloId);
-		};
+	public Optional<Integer> getModeloId() {
+		var moduloId = this.filter.containsKey("mdo_id")
+			? Integer.parseInt(this.filter.get("mdo_id"))
+			: null;
+		return Optional.ofNullable(moduloId);
 	}
 
-	public Specification<Veiculo> withTipo(VeiculoEnum tipo) {
-		return (root, query, builder) -> builder.equal(root.get("tipo"), tipo.codigo);
+	public Optional<String> getTipo() {
+		var tipo = this.filter.get("vco_tipo");
+		return Optional.ofNullable(tipo);
 	}
 
-	public Specification<Veiculo> withAno(Integer ano) {
-		return (root, query, builder) -> builder.equal(root.get("ano"), ano);
+	public Optional<Integer> getAno() {
+		var ano = this.filter.containsKey("vco_ano")
+			? Integer.parseInt(this.filter.get("vco_ano"))
+			: null;
+		return Optional.ofNullable(ano);
 	}
 }
