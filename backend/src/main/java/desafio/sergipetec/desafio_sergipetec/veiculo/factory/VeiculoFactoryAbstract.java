2f7fef59bb08f29d797568b2a0c662688106980f
Anailson Mota [W10] <anailsonmota@moobi.com.br>
2025-02-03 02:26:51 -0300

    Corrige validação quando os campos são nulos ou string vazia

--- backend/src/main/java/desafio/sergipetec/desafio_sergipetec/veiculo/factory/VeiculoFactoryAbstract.java
+++ backend/src/main/java/desafio/sergipetec/desafio_sergipetec/veiculo/factory/VeiculoFactoryAbstract.java
@@ -10,6 +10,7 @@ import desafio.sergipetec.desafio_sergipetec.modelo.Modelo;
 import desafio.sergipetec.desafio_sergipetec.modelo.ModeloDAO;
 import desafio.sergipetec.desafio_sergipetec.modelo.ModeloService;
 import desafio.sergipetec.desafio_sergipetec.veiculo.Veiculo;
+import desafio.sergipetec.desafio_sergipetec.veiculo.VeiculoEnum;
 import jakarta.persistence.EntityNotFoundException;
 
 public abstract class VeiculoFactoryAbstract implements VeiculoFactoryInterface {
@@ -24,19 +25,19 @@ public abstract class VeiculoFactoryAbstract implements VeiculoFactoryInterface 
 
 	@Override
 	public void validateRequired(HashMap<String, String> map) throws InvalidParameterException {
-		if (!map.containsKey("fbe_id")) {
+		if (!this.hasValue("fbe_id", map)) {
 			throw new InvalidParameterException("Informe o fabricante");
 		}
 
-		if (!map.containsKey("mdo_id")) {
+		if (!this.hasValue("mdo_id", map)) {
 			throw new InvalidParameterException("Informe o modelo");
 		}
 
-		if (!map.containsKey("vco_ano")) {
+		if (!this.hasValue("vco_ano", map)) {
 			throw new InvalidParameterException("Informe o ano");
 		}
 
-		if (!map.containsKey("vco_preco")) {
+		if (!this.hasValue("vco_preco", map)) {
 			throw new InvalidParameterException("Informe o preço");
 		}
 
@@ -59,6 +60,7 @@ public abstract class VeiculoFactoryAbstract implements VeiculoFactoryInterface 
 			veiculo.setModelo(this.getModelo(map));
 			veiculo.setAno(Integer.parseInt(map.get("vco_ano")));
 			veiculo.setPreco(Double.valueOf(map.get("vco_preco")));
+			veiculo.setTipo(VeiculoEnum.parse(Integer.parseInt(map.get("vco_tipo"))));
 		} catch (Exception e) {
 			throw new InvalidParameterException(e.getMessage());
 		}
@@ -67,22 +69,31 @@ public abstract class VeiculoFactoryAbstract implements VeiculoFactoryInterface 
 
 	@Override
 	public Veiculo replace(Veiculo veiculo, HashMap<String, String> map) throws InvalidParameterException {
-		if (map.containsKey("fbe_id")) {
+		if (this.hasValue("vco_tipo", map)) {
+			try {
+				var tipo = VeiculoEnum.parse(Integer.parseInt(map.get("vco_tipo")));
+				veiculo.setTipo(tipo);
+			} catch (Exception e) {
+				throw new InvalidParameterException(e.getMessage());
+			}
+		}
+
+		if (this.hasValue("fbe_id", map)) {
 			var fabricante = this.getFabricante(map);
 			veiculo.setFabricante(fabricante);
 		}
 
-		if (map.containsKey("mdo_id")) {
+		if (this.hasValue("mdo_id", map)) {
 			var modelo = this.getModelo(map);
 			veiculo.setModelo(modelo);
 		}
 
-		if (map.containsKey("vco_ano")) {
+		if (this.hasValue("vco_ano", map)) {
 			var ano = Integer.parseInt(map.get("vco_ano"));
 			veiculo.setAno(ano);
 		}
 
-		if (map.containsKey("vco_preco")) {
+		if (this.hasValue("vco_preco", map)) {
 			var preco = Double.valueOf(map.get("vco_preco"));
 			veiculo.setPreco(preco);
 		}
@@ -97,5 +108,10 @@ public abstract class VeiculoFactoryAbstract implements VeiculoFactoryInterface 
 	protected Modelo getModelo(HashMap<String, String> map) throws EntityNotFoundException {
 		var modeloId = Integer.parseInt(map.get("mdo_id"));
 		return this.modeloService.get(modeloId);
+	}
+
+	private boolean hasValue(String key, HashMap<String, String> map) {
+		var value = map.get(key);
+		return value != null && value.length() > 0;
 	}
 }
